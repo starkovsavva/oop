0f57934b1ab91a6151c12a2b57c85d614abe30d5
starkovsavva <starkovsavva52@gmail.com>
2024-10-04 01:28:17 +0300

    1

---
+++ models/headers/ShipManager.h
@@ -0,0 +1,16 @@
+//
+// Created by sdf on 03.10.2024.
+//
+
+#ifndef UNTITLED1_SHIPMANAGER_H
+#define UNTITLED1_SHIPMANAGER_H
+
+
+
+class ShipManager {
+
+};
+
+
+
+#endif //UNTITLED1_SHIPMANAGER_H
---
+++ models/sources/ShipManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sdf on 03.10.2024.
+//
+
+#include "ShipManager.h"
